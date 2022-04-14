4e589f2d8defba12cf2912f88ad33813f978f85b
Reza Mohseni <49490512+mohsenireza@users.noreply.github.com>
2022-04-14 13:19:33 -0500

    Edit fetchPolicy of ApolloClient

--- src/config/apolloClient.js
+++ src/config/apolloClient.js
@@ -1,23 +1,14 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { API_BASE_URL } from '../constants';
 
-const cache = new InMemoryCache({
-  typePolicies: {
-    Product: {
-      fields: {
-        attributes: {
-          merge(existing, incoming) {
-            return { ...existing, ...incoming };
-          },
-        },
-      },
-    },
-  },
-});
-
 const client = new ApolloClient({
   uri: API_BASE_URL,
-  cache,
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    query: {
+      fetchPolicy: 'no-cache',
+    },
+  },
 });
 
 export { client };
